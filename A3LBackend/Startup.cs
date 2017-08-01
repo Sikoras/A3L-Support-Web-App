b93a4b6870d7b2f41e8811e7202fdf1dc4129097
ThomasAgambar <agbrth@hotmail.co.uk>
2017-08-01 15:55:42 +0100

    Add project files.

---
+++ A3LBackend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A3LBackend.Startup))]
+namespace A3LBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
